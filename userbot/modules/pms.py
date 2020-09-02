ac585b674b9b8477921fcdbe3d09a601cbc76270
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-09-02 17:31:57 +0530

    Update pms.py

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -251,7 +251,7 @@ async def blockpm(block):
     else:
         await block.client(BlockRequest(block.chat_id))
         aname = await block.client.get_entity(block.chat_id)
-        await block.edit("`You've been blocked 😡!`")
+        await block.edit("`You've been blocked! 😡`")
         name0 = str(aname.first_name)
         uid = block.chat_id
 
