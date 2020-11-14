448fa440bc3052373a50ab41d14bcb10f24f062d
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-11-14 16:55:21 +0530

    Update pms.py

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -313,6 +313,24 @@ async def monito_p_m_s(event):
                     )
 
 
+                
+@register(outgoing=True, incoming=True, func=lambda e: e.mentioned)
+async def log_tagged_messages(event):
+    hmm = await event.get_chat()
+        
+    if PM_LOGGR_BOT_API_ID:
+        sender = await event.get_sender()
+        await asyncio.sleep(5)
+        if not event.is_private:
+            await event.client.send_message(
+                PM_LOGGR_BOT_API_ID,
+                f"#TAGS \n<b>Group : </b><code>{hmm.title}</code>\
+                        \n<b>Message : </b><a href = 'https://t.me/c/{hmm.id}/{event.message.id}'> link</a>",
+                parse_mode="html",
+                link_preview=False,
+            )
+        
+            
 
 @register(pattern="^.nolog(?: |$)(.*)")
 async def approve_p_m(event):
